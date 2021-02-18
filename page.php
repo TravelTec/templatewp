60a4c6ca7f0ddd9e0d06569ae4c72ba1f3ab3ac0
Raabe Sampaio <raabe@montenegroev.com.br>
2021-02-17 21:38:35 -0300

    v1.0.21 - Correção de modelo
    Alteração da página modelo padrão de modelo de blog para página largura padrão com faixa e sem barra lateral

--- page.php
+++ page.php
@@ -55,93 +55,7 @@ $the_query = new WP_Query ( array( 'post_type'=>'post' ) );
 <!-- Blog Section with Sidebar -->
 
 <div class="page-builder"> 
-
-		<?php if ( $the_query->have_posts() ) { ?>
-	<div class="container">
-
-		<div class="row"> 
-
-			<!-- Blog Area -->
-
-			<div class="col-md-8" >
-
-			<?php 
-
-		while ( $the_query->have_posts() ) { 
-
-			$the_query->the_post();  ?> 
-			<div class="media">						
-
-			<?php
-
-			
-
-			 if(!is_home() && !is_page_template('blog-masonry.php'))
-
-			{
-
-			appointment_aside_meta_content(); 
-
-			}
-
-			elseif(is_home())
-
-			{
-
-			appointment_aside_meta_content(); 
-
-			}
-
-			
-
-			?>
-
-		<div class="media-body">
-
-				<?php // Check Image size for fullwidth template
-
-				 appointment_post_thumbnail('','img-responsive'); 
-				?>
-
-				<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-
-		        <?php  
-
-                // call editor content of post/page	
-
-				the_content( __( 'Read More' , 'appointment' ) );
-
-				wp_link_pages( );
-
-		       ?>
-
-		</div>
-
-	</div>
-	<br><br>
-
-			<?php } ?> 
-
-			</div>
-
-			<!-- /Blog Area -->			
-
-			
-
-			<!--Sidebar Area-->
-
-			<div class="col-md-4">
-
-			<?php get_sidebar(); ?>	
-
-			</div>
-
-			<!--Sidebar Area-->
-
-		</div>
-
-	</div>
-		<?php }else{  // show comments ?>
+ 
 	<?php if ($_SERVER['REQUEST_URI'] == '/checkout/') { ?>
 		<div class="container-fluid">
 	<?php }else{ ?>
@@ -162,8 +76,7 @@ $the_query = new WP_Query ( array( 'post_type'=>'post' ) );
 
 			<!-- /Blog Area --> 
 
-		</div>
-	<?php } ?>
+		</div> 
 
 
 	</div>
