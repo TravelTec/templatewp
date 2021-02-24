9a186a55ab38ecee019a94dedf2e0aed56b0dd9b
TravelTec <59958315+TravelTec@users.noreply.github.com>
2021-02-24 11:36:52 -0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -49,7 +49,7 @@ function my_theme_update_checker_setting() {
 
     // (Opcional) Set the branch that contains the stable release.
 
-    $myUpdateChecker->setBranch('main');
+    $myUpdateChecker->setBranch('vouchertec-template-beta');
 
 }
 
