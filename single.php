71215dd1c474034871560fba16f4a123ab0d28f0
Raabe Sampaio <raabe@montenegroev.com.br>
2021-03-10 11:16:41 -0300

    v1.1.26 - Blog
    Correção da exibição do post individual do blog

--- single.php
+++ single.php
@@ -26,7 +26,7 @@ get_template_part('index','banner'); ?>
 
 		while(have_posts()) { the_post();
 
-		get_template_part( 'content', get_post_format() ); ?>
+		get_template_part( 'content-single', get_post_format() ); ?>
 
 				<!--Blog Author-->
 
